f7da2582bedcaad917e08190e08a640a0f92ec34
PC-20181118AWSO\HuangGang <462256272@qq.com>
2019-10-20 21:52:53 +0800

    《12_尚硅谷_React全栈项目_Login组件_Form表单》 这里拼接了login的静态页面

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,13 +1,50 @@
 import React, {Component} from 'react';
+import "./login.less";
+import logo from "./images/logo.png"; // jsx中，需要这样引入图片。
+import {Form, Icon, Input, Button} from 'antd';
 
 /**
  * 登陆的路由组件，一级路由。
  */
 class Login extends Component {
+
+    /**
+     *
+     */
+    handleSubmit = (event) => {
+
+    };
+
     render() {
         return (
-            <div>
-                Login
+            <div className="login">
+                <header className="login-header">
+                    <img src={logo} alt="logo"/>
+                    <h1>React项目：后台管理系统</h1>
+                </header>
+                <section className="login-content">
+                    <h2>用户登录</h2>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Form.Item>
+                            <Input
+                                prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                placeholder="用户名"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Input
+                                prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                type="password"
+                                placeholder="密码"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                登 录
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </section>
             </div>
         );
     }
