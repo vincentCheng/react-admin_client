27b42242d6b67185826fb47f86b97e9199f4dbeb
PC-20181118AWSO\HuangGang <462256272@qq.com>
2019-10-20 18:45:50 +0800

    根据官网做出初始配置，修改了主题。

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
+/*
+* 入口js
+* */
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from "react-dom";
+// import 'antd/dist/antd.min.css';// 这里引入ant的样式
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App/>, document.getElementById('root'))
