32f9e8d003eb5531e9598207ae41eff8e5269a2d
PC-20181118AWSO\HuangGang <462256272@qq.com>
2019-10-24 18:54:15 +0800

    《39_尚硅谷_React全栈项目_jsonp解决ajax跨域的原理》

--- src/api/index.js
+++ src/api/index.js
@@ -24,6 +24,18 @@ export const reqAddUser = (username, password, phone = '', email = '', role_id =
 * jsonp请求的接口函数
 * 不能使用ajax
 * 需要使用jsonp请求，这个是为了适应百度天气的连接而使用的。
+*
+* - jsonp功能：
+	- 1、用来解决ajax的跨域请求问题。只能使用“GET”请求。
+	- 2、本质是一般的get请求，不是ajax请求。
+	- 3、基本原理：
+			- 浏览器端：
+				动态生成script请求后台接口（src就是接口的url）。
+				定义好用于接收响应数据的函数（fn），并且将函数名通过请求参数提交到服务端。
+			- 服务器端：
+				接收到请求处理产生结果数据后，返回一个函数调用的js代码，并且将结果数据作为实参传入函数调用。
+			- 浏览器端：
+				收到响应自动执行函数调用的js代码，也就执行了提前定义好的回调函数，并且得到需要的结果数据。
 * */
 export const reqWeather = (city) => {
     let url = ReqWeatherUrlParts[0] + city + ReqWeatherUrlParts[1];
