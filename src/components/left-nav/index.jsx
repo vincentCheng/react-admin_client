331bb9ed19d5627be6ca055fbea80d7ceedcc3d3
PC-20181118AWSO\HuangGang <462256272@qq.com>
2019-10-24 09:41:41 +0800

    《32_尚硅谷_React全栈项目_动态显示菜单列表_map()和递归》 map+递归调用

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -3,10 +3,48 @@ import logo from "../../assets/images/logo.png";
 import './index.less';
 import {Link} from "react-router-dom";
 import {Menu, Icon} from 'antd';
+import {menuConfig} from "../../config/menuConfig";
 
 const {SubMenu} = Menu;
 
 class Index extends Component {
+    /**
+     * 根据menu的数据生成对应的标签
+     *
+     * 使用map+递归调用
+     * @param menuList
+     */
+    static getMenuNodes(menuList) {
+        return menuList.map(item => {
+            if (!item.children) {
+                return (
+                    <Menu.Item key={item.key}>
+                        <Link to={item.key}>
+                            <Icon type={item.icon}/>
+                            <span>{item.title}</span>
+                        </Link>
+                    </Menu.Item>
+                )
+            }
+
+            // 如果有子元素
+            // 循环使用，继续生成子元素
+            return (
+                <SubMenu
+                    key={item.key}
+                    title={
+                        <span>
+								<Icon type={item.icon}/>
+								<span>{item.title}</span>
+							</span>
+                    }
+                >
+                    {Index.getMenuNodes(item.children)}
+                </SubMenu>
+            )
+        })
+    }
+
     render() {
         return (
             <div>
@@ -17,68 +55,16 @@ class Index extends Component {
                     </Link>
                 </div>
 
-
                 <Menu
                     mode="inline"
                     theme="dark"
                 >
-                    <Menu.Item key="/home">
-                        <Link to='/home'>
-                            <Icon type="pie-chart"/>
-                            <span>首页</span>
-                        </Link>
-                    </Menu.Item>
-
-                    <SubMenu
-                        key="goods"
-                        title={
-                            <span>
-								<Icon type="mail"/>
-								<span>商品</span>
-							</span>
-                        }
-                    >
-                        <Menu.Item key="/category">
-                            <Link to='/category'>
-								<span>
-									<Icon type="mail"/>
-									<span>品类管理</span>
-								</span>
-                            </Link>
-                        </Menu.Item>
-                        <Menu.Item key="/product"><span>
-                            <Link to='/product'>
-								<span>
-									<Icon type="mail"/>
-									<span>商品管理</span>
-								</span>
-							</Link>
-						</span>
-                        </Menu.Item>
-                    </SubMenu>
-
-                    <Menu.Item key="/user">
-                        <Link to='/user'>
-								<span>
-									<Icon type="pie-chart"/>
-									<span>用户管理</span>
-								</span>
-                        </Link>
-                    </Menu.Item>
-
-                    <Menu.Item key="/role">
-                        <Link to='/role'>
-								<span>
-									<Icon type="pie-chart"/>
-									<span>角色管理</span>
-								</span>
-                        </Link>
-                    </Menu.Item>
+                    {Index.getMenuNodes(menuConfig)}
                 </Menu>
-
             </div>
         );
     }
+
 }
 
 export default Index;
